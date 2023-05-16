c38c1d8a6c06956c2e478f0ac626642b43b0dc79
Lucas Mösch <lucasmoesch@live.de>
2023-05-17 01:24:43 +0200

    Added preliminary nmea class

---
+++ nmea0183_impl.hpp
@@ -0,0 +1,16 @@
+/* Copyright (c) 2021, Lucas Mösch
+ * All Rights Reserved.
+ */
+// ----------------------------------------------------------------------------
+
+#include "nmea0183.hpp"
+
+namespace modm
+{
+
+template <typename Uart>
+NMEA0183<Uart>::NMEA0183()
+{
+
+}
+}
